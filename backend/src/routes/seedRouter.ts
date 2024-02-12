83ea57ab1ded332da638367f55622b22f8834814
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-02-12 14:30:06 +0200

    First commit

---
+++ backend/src/routes/seedRouter.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { seedData } from "../controllers/seedData";
+
+
+
+
+const seedRouter=express.Router();
+
+seedRouter.get('/',seedData);
+
+export default seedRouter;
