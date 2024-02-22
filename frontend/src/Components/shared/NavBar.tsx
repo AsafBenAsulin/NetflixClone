114d9fac63acc275a9415541d0a1233339297580
AsafBenAsulin <156786762+AsafBenAsulin@users.noreply.github.com>
2024-02-22 15:52:46 +0200

    Nav bar background will be black when scrolling

--- frontend/src/Components/shared/NavBar.tsx
+++ frontend/src/Components/shared/NavBar.tsx
@@ -1,14 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Cookies from "js-cookie";
 import { USER_SIGNOUT } from '@/Actions';
 import { Link, useNavigate } from 'react-router-dom';
 import { User } from '@/user';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-
 const NavBar = () => {
     const navigate = useNavigate();
     const { state: { userInfo }, dispatch: ctxDispatch } = useContext(User);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const clickHandler = () => {
         Cookies.remove("userInfo");
@@ -16,9 +29,10 @@ const NavBar = () => {
         navigate("/");
     }
 
+    const navbarClass = scrollPosition > 0 ? "bg-black" : "bg-gradient-to-b from-black to-transparent";
 
     return (
-        <div className="p-2 bg-gradient-to-b from-black to-transparent fixed top-0 left-0 right-0 z-10 text-white">
+        <div className={`p-2 fixed top-0 left-0 right-0 z-10 text-white transition-all duration-500 ${navbarClass}`}>
             <div className="container mx-auto flex justify-between items-center">
                 <div className='flex justify-between-left items-center space-x-6'>
                     <img src="\assets\Netflix-Logo.wine.svg" alt="Netflix Logo" className="w-22 mr-2" />
@@ -41,7 +55,6 @@ const NavBar = () => {
                 </div>
             </div>
         </div>
-
     );
 }
 
